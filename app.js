db13972f20b164474d9ad970c10a89ecad64223d
TheCloudArchitect <dakotaclason@gmail.com>
2019-11-04 08:05:25 -0600

    added jurassic world book

--- app.js
+++ app.js
@@ -33,6 +33,10 @@ const books = [
       title: 'Jurassic City',
       author: 'Michael Scott',
     },
+    {
+      title: 'Jurassic World',
+      author: 'Stephen King',
+    },
   ];
   // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
